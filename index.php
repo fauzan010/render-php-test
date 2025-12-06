e4105c5a184d3a7d46c1d1c52b8cde21b7063067
fauzan010 <158558285+fauzan010@users.noreply.github.com>
2025-12-06 12:46:45 +0700

    Change response format to plain text

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-header("Content-Type: application/json");
-
-echo json_encode([
-    "status" => "success",
-    "message" => "API Render PHP berjalan",
-    "time" => date("Y-m-d H:i:s")
-]);
+header("Content-Type: text/plain");
+echo "OK " . time();
